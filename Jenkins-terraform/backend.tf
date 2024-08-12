dd3115fe84fa9d476c50c6e5c83664ef47572e42
psslalitha <poojitha2527@gmail.com>
2024-08-12 12:27:28 +0530

    Update backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3-final-111" # Replace with your actual S3 bucket name
+    bucket = "devsecops-p" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
