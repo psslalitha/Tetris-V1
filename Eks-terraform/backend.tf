d74ae7a3edb8fad9bbee87c027624a3fe31e48fe
psslalitha <poojitha2527@gmail.com>
2024-08-12 15:24:54 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3-final-111" # Replace with your actual S3 bucket name
+    bucket = "argocd-p" # Replace with your actual S3 bucket name
     key    = "Eks/terraform.tfstate"
     region = "ap-south-1"
   }
